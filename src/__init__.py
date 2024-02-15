087367e6f2683f1ad16209f0850be9c7cc8d9721
jemorrison <morrison@as.arizona.edu>
2024-02-15 16:08:56 -0700

    clean up

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+from .core_schema import *
+from .esc_image_schemas import *
+from .esc_visit_schemas import *
+from .schema_utils import *
+
+from . import _version
+__all__ = ['__version__']
+
+
+__version__ = _version.version
