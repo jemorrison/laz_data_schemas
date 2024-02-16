0d8d1153edd96c63e971d64f70be7fe60d4b7e50
jemorrison <morrison@as.arizona.edu>
2024-02-16 14:45:29 -0700

    fixed distribution package

--- src/__init__.py
+++ src/__init__.py
@@ -3,8 +3,3 @@ from .esc_image_schemas import *
 from .esc_visit_schemas import *
 from .schema_utils import *
 
-from . import _version
-__all__ = ['__version__']
-
-
-__version__ = _version.version
